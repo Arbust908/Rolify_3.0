66289e56fe88c7d4526d287952a135e82594a6e4
Arbust0 <Arbust908@gmail.com>
2017-08-26 17:49:51 -0300

    Design?
    -Ejample Design
    -Others...

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="main.css">
+  <script src="https://use.fontawesome.com/455b8c317a.js"></script>
+  <title>Rolify TE</title>
+</head>
+
+
+<body class="container">
+    <header class="navbar">
+      <nav>
+        <img src="" alt="Logo">
+        <h2>Rolify</h2>
+        <img src="" alt="Random">
+      </nav>
+    </header>
+
+    <nav class="sidebar">
+      <div class="category">
+        <h3>Tables</h3>
+        <br>
+      </div>
+      <div class="category">
+        <h5>Starter</h5>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+        <br>
+      </div>
+      <div class="category">
+        <h5>Weapons</h5>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </div>
+      <div class="category">
+        <h5>NPC</h5>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </div>
+
+    </nav>
+
+    <footer class="footer">
+      <div class="rights">
+        <hr>
+        <p>Rights to the coding machine (Nico)+(Pancho)</p>
+        <hr>
+      </div>
+      <i class="fa fa-google" aria-hidden="true"></i>
+      <i class="fa fa-github-alt" aria-hidden="true"></i>
+      <i class="fa fa-slack" aria-hidden="true"></i>
+      <i class="fa fa-youtube-play" aria-hidden="true"></i>
+      <i class="fa fa-reddit-alien" aria-hidden="true"></i>
+    </footer>
+
+    <main class="content">
+      <div class="roll">
+        <img src="" alt="imagen">
+        <h4>Titulo</h4>
+      </div>
+    </main>
+</body>
+
+</html>
