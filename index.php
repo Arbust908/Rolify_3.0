b3f7566054214426b0fe99876d77443800df0f2b
Arbust0 <Arbust908@gmail.com>
2017-09-01 11:02:50 -0300

    Styling
    -Added CSS
    -Added Queries
    -Added Magic
    -Profit

--- index.php
+++ index.php
@@ -3,30 +3,36 @@
 
 <head>
   <meta charset="utf-8">
+  <link href="https://fonts.googleapis.com/css?family=Cinzel:400,900&amp;subset=latin-ext" rel="stylesheet">
   <link rel="stylesheet" href="main.css">
+  <link rel="stylesheet" href="query_tablet.css">
+  <link rel="stylesheet" href="query_laptop.css">
+
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://use.fontawesome.com/455b8c317a.js"></script>
+  <link rel="shortcut icon" href="icon.ico">
   <title>Rolify TE</title>
 </head>
 
 
-<body class="flex-cont">
+<body>
   <div class="container">
     <header class="navbar">
       <div class="flex-magic">
-        <img src="" alt="Logo">
+        <a class="logo prime"><img src="logo.png" alt="Logo"></a>
       </div>
       <div class="flex-magic">
-        <h2>Rolify</h2>
+        <h2 class="nombre">Rolify</h2>
       </div>
         <div class="flex-magic">
-          <img src="" alt="Random">
+          <a class="logo rand"><img src="unnamed.png" alt="Random"></a>
         </div>
     </header>
 
     <nav class="sidebar">
-      <div class="category">
+      <div class="flex-magic">
         <h3>Tables</h3>
+        <i class="fa fa-angle-double-down dropdown" aria-hidden="true"></i>
         <br>
       </div>
       <div class="category">
@@ -57,34 +63,6 @@
 
     </nav>
 
-    <footer class="footer">
-
-      <div class="flex-magic">
-        <div class="rights">
-          <hr>
-          <p>Rights to the coding machine (Nico)+(Pancho)</p>
-          <hr>
-        </div>
-      </div>
-
-      <div class="flex-magic">
-
-        <div class="masmagia">
-            <a class="google social-icon"><i class="fa fa-google" aria-hidden="true"></i></a>
-
-            <a class="github social-icon"><i class="fa fa-github-alt" aria-hidden="true"></i></a>
-
-            <a class="slack social-icon"><i class="fa fa-slack" aria-hidden="true"></i></a>
-
-            <a class="youtube social-icon"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
-
-            <a class="reddit social-icon"><i class="fa fa-reddit-alien" aria-hidden="true"></i></a>
-
-        </div>
-
-      </div>
-    </footer>
-
     <main class="content">
       <div class="flex-cont">
 
@@ -105,6 +83,29 @@
 
       </div>
     </main>
+
+    <footer class="footer">
+
+      <div class="flex-magic">
+        <div class="rights">
+          <hr>
+          <p>Rights to the coding machine (Nico)+(Pancho)</p>
+          <hr>
+        </div>
+      </div>
+
+      <div class="flex-magic">
+        <div class="masmagia">
+            <a class="google social-icon"><i class="fa fa-google" aria-hidden="true"></i></a>
+            <a class="github social-icon"><i class="fa fa-github-alt" aria-hidden="true"></i></a>
+            <a class="slack social-icon"><i class="fa fa-slack" aria-hidden="true"></i></a>
+            <a class="youtube social-icon"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
+            <a class="reddit social-icon"><i class="fa fa-reddit-alien" aria-hidden="true"></i></a>
+        </div>
+      </div>
+
+    </footer>
+
   </div>
 
 </body>
