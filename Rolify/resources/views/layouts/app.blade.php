b373d605fd1db772067d44b02232fc49eaba65f4
Arbust0 <Arbust908@gmail.com>
2017-09-06 17:20:30 -0300

    New Sassy SASS
    -Added New Layout
    -Added SASS code

--- Rolify/resources/views/layouts/app.blade.php
+++ Rolify/resources/views/layouts/app.blade.php
@@ -4,77 +4,142 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+      <script src="https://use.fontawesome.com/455b8c317a.js"></script>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <title>{{ config('app.name', 'Rolify') }}</title>
+    <link rel="shortcut icon" href="icon.ico">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
+  {{-- cerrar --}}
+  <div class="container">
+      {{-- cerrar --}}
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
 
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
+<!-- Barra de Nav -->
+    <header class="navbar">
+      <div class="flex-magic">
+        <a href="index.php" class="logo prime">
+          <img src="logo.png" alt="Logo">
+        </a>
+      </div>
+      <div class="flex-magic">
+        <h2 class="nombre">Rolify</h2>
+      </div>
+      <div class="flex-magic">
+          {{-- <a href="index.php" class="logo rand">
+            <img src="unnamed.png" alt="Random">
+          </a> --}}
+          <!-- Authentication Links -->
+          @if (Auth::guest())
+              <li><a href="{{ route('login') }}">Login</a></li>
+              <li><a href="{{ route('register') }}">Register</a></li>
+          @else
+              <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                      {{ Auth::user()->name }} <span class="caret"></span>
+                  </a>
+              </li>
+              <li>
+                  <a href="{{ route('logout') }}"
+                      onclick="event.preventDefault();
+                               document.getElementById('logout-form').submit();">
+                      Logout
+                  </a>
 
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                      {{ csrf_field() }}
+                  </form>
+              </li>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+          @endif
+        </div>
+    </header>
 
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
+<!-- Barra Lateral -->
+    <nav class="sidebar">
+      <div class="flex-magic">
+        <h3>Tables</h3>
+        <i class="fa fa-angle-double-down dropdown" aria-hidden="true"></i>
+        <br>
+      </div>
+      <div class="category">
+        <h5>Starter</h5>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+        <br>
+      </div>
+      <div class="category">
+        <h5>Weapons</h5>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </div>
+      <div class="category">
+        <h5>NPC</h5>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </div>
 
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
-            </div>
-        </nav>
+    </nav>
 
+<!-- Contenido Principal -->
+    <main class="content">
+      <div class="flex-cont">
         @yield('content')
-    </div>
+      </div>
+    </main>
+
+<!-- Footer -->
+    <footer class="footer">
+
+      <div class="flex-magic">
+        <div class="rights">
+          <hr>
+          <p>Rights to the coding machine (Nico)+(Pancho)</p>
+          <hr>
+        </div>
+      </div>
+
+      <div class="flex-magic">
+        <div class="masmagia">
+            <a href="http://panchoblanco.com.ar/" class="google social-icon">
+              <i class="fa fa-google" aria-hidden="true"></i>
+            </a>
+            <a href="http://panchoblanco.com.ar/" class="github social-icon">
+              <i class="fa fa-github-alt" aria-hidden="true"></i>
+            </a>
+            <a href="http://panchoblanco.com.ar/" class="slack social-icon">
+              <i class="fa fa-slack" aria-hidden="true"></i>
+            </a>
+            <a href="http://panchoblanco.com.ar/" class="youtube social-icon">
+              <i class="fa fa-youtube-play" aria-hidden="true"></i>
+            </a>
+            <a href="http://panchoblanco.com.ar/" class="reddit social-icon">
+              <i class="fa fa-reddit-alien" aria-hidden="true"></i>
+            </a>
+        </div>
+      </div>
+
+    </footer>
+
+  </div>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
+
 </body>
 </html>
